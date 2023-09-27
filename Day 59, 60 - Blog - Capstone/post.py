73b3e459191316f04eb1142283390bc95a2b4315
Moaaz Ahmad <74923982+mak-beep@users.noreply.github.com>
2023-09-28 01:48:41 +0500

    Delete Day 59, 60 - Blog - Capstone directory

--- Day 59, 60 - Blog - Capstone/post.py
+++
@@ -1,6 +0,0 @@
-class Post:
-    def __init__(self, new_post):
-        self.title = new_post['title']
-        self.body = new_post['body']
-        self.id = new_post['id']
-        self.subtitle = new_post['subtitle']
